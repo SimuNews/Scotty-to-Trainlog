a23e2f7f776f85cef3fc58c14f34de8b879366f3
SimuNews <teardropdev@outlook.com>
2024-11-07 17:49:10 +0100

    STT-2 Try first dom changes

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,3 +1,29 @@
+// import { browser } from "webextension-polyfill-ts";
+
 console.log('helloworld from content script');
+
+// function saveData(result: any) {
+//     console.log(result);
+// }
+
+// browser.webRequest.onCompleted.addListener(
+//     saveData,
+//     {
+//         urls: ["http://*/*","https://*/*"],
+//         types: ['xmlhttprequest']
+//     });
+
+window.addEventListener("resize", (ev: any) => {
+    console.log("resize");
+    const doc: Document = ev.originalTarget?.document;
+    const elements = doc.querySelectorAll(".lyr_tpActionButtons");
+    elements.forEach(e => e.appendChild(
+        doc.createElement("button")
+            .appendChild(
+                doc.createElement("span")
+            )
+        )
+    );
+});
 
 export {};
